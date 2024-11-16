9874ca7102a8de4383f05bf4be5906e98b0a8537
shiyinli <lsyin85@gmail.com>
2024-11-16 11:14:41 +0100

    First commit

--- src/main/java/org/example/demo3/HelloController.java
+++ src/main/java/org/example/demo3/HelloController.java
@@ -2,13 +2,46 @@ package org.example.demo3;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
+import java.util.Random;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField digitInput;
+    @FXML
+    private Label resultLabel;
+    @FXML
+    private Label statusLabel;
+
+    private String secretNumber;
+    private StringBuilder currentGuess;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize(){
+        generateSeretNumber();
+        currentGuess = new StringBuilder("______");
+        statusLabel.setText("Guess a digit for position 1!");
     }
+
+    private void generateSeretNumber(){
+        Random random = new Random();
+        secretNumber = String.format("%06d", random.nextInt(1_000_000));
+        /*System.out.println("Secret Number: "+secretNumber);*/
+    }
+
+    @FXML
+    public void handleSubmit(){
+        String input = digitInput.getText();
+        if(input.length()!=1 || !Character.isDigit(input.charAt(0))){
+            resultLabel.setText("Enter a single digit (0-9)!");
+            return;
+        }
+
+        char guessedDigit = input.charAt(0);
+        int position=currentGuess.indexOf("_");
+
+    }
+
 }
