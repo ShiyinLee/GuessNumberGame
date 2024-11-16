79dfa758296c741407e982d2047a41851ff8b0e6
shiyinli <lsyin85@gmail.com>
2024-11-16 12:25:04 +0100

    Working code with 6 digit

--- src/main/java/org/example/demo3/HelloApplication.java
+++ src/main/java/org/example/demo3/HelloApplication.java
@@ -2,6 +2,7 @@ package org.example.demo3;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,8 +12,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+        stage.setTitle("Guess the Number Game");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/example/demo3/HelloController.java
+++ src/main/java/org/example/demo3/HelloController.java
@@ -16,13 +16,13 @@ public class HelloController {
     private Label statusLabel;
 
     private String secretNumber;
-    private StringBuilder currentGuess;
+//    private StringBuilder currentGuess;
 
     @FXML
     public void initialize(){
         generateSeretNumber();
-        currentGuess = new StringBuilder("______");
-        statusLabel.setText("Guess a digit for position 1!");
+//        currentGuess = new StringBuilder("______");
+        statusLabel.setText("Guess the 6-digit number!");
     }
 
     private void generateSeretNumber(){
@@ -34,14 +34,50 @@ public class HelloController {
     @FXML
     public void handleSubmit(){
         String input = digitInput.getText();
-        if(input.length()!=1 || !Character.isDigit(input.charAt(0))){
-            resultLabel.setText("Enter a single digit (0-9)!");
+        if(input.length()!=6 || !input.matches("\\d{6}")){
+            resultLabel.setText("Enter a valid 6-digit number");
             return;
         }
 
-        char guessedDigit = input.charAt(0);
-        int position=currentGuess.indexOf("_");
+//        char guessedDigit = input.charAt(0);
+//        int position=currentGuess.indexOf("_");
+        StringBuilder feedback=new StringBuilder();
+        boolean[] matchedInSecret = new boolean[6];
 
+        for(int i=0;i<6;i++){
+            if(input.charAt(i)==secretNumber.charAt(i)){
+                feedback.append("+");
+                matchedInSecret[i]=true;
+            }else{
+                feedback.append("_");
+            }
+        }
+
+        for(int i=0; i<6; i++){
+            if(feedback.charAt(i)!='+'){
+                boolean found=false;
+                for(int j=0;j<6;j++){
+                    if(!matchedInSecret[j]&&input.charAt(i)==secretNumber.charAt(j)){
+                        feedback.setCharAt(i,'*');
+                        matchedInSecret[j]=true;
+                        found=true;
+                        break;
+                    }
+                }
+                if(!found){
+                    feedback.setCharAt(i,'-');
+                }
+            }
+        }
+         resultLabel.setText("Feedback: "+feedback);
+        if(feedback.toString().equals("++++++")){
+            statusLabel.setText("Congratulations! You guessed the number: "+secretNumber);
+            digitInput.setDisable(true);
+        }else{
+            statusLabel.setText("Try again!");
+        }
+
+        digitInput.clear();
     }
 
 }
