4cfaf0a4d2afeaab8324565057d422f94d6b5ac1
shiyinli <lsyin85@gmail.com>
2024-11-16 10:38:47 +0100

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.example.demo3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.demo3 to javafx.fxml;
+    exports org.example.demo3;
+}
